0a1aa658ee7c688676ebf9218de8122f07df4d95
Trent Moss <trentmoss05@gmail.com>
2019-01-28 15:48:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,18 @@ class Song
 
   attr_accessor :title, :artist
 
-  #def initialize(title)
-  #  @title = title
-  #end
+  def initialize(title)
+    @title = title
+  end
+
+end
+
+class Artist
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 
 end
